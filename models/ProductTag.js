c9ed0c7a013af57c440a3e64e88236be836cb9a9
javimarashall <jamondragon@ucdavis.edu>
2021-04-27 20:53:46 -0700

    models done

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,8 +5,28 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
+   // define columns
   {
-    // define columns
+   id: {
+     type: DataTypes.INTEGER, //sets the type to integer
+     allowNull: false, //doesn't allow nulls
+     primaryKey: true, //set as the id as the primary key
+     autoIncrement: true, //sets auto increment
+   },
+   product_id: {
+     type: DataTypes.INTEGER, //sets the type as integer
+     references: {
+       model: 'Product', //reference the product model
+       key: 'id', //use the id from product model as the reference key
+     },
+   },
+   tag_id: {
+     type: DataTypes.INTEGER, //sets the type to integer
+     references: {
+       model: 'Tag', //references the tag model
+       key: 'id', //use the id for the reference
+     }
+   }
   },
   {
     sequelize,

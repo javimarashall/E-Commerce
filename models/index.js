1df516861c84e45f347a6607ee5a24dc76b6d25c
javimarashall <jamondragon@ucdavis.edu>
2021-04-27 21:53:09 -0700

    models index

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, { //product belongs to category
+  foreignKey: 'category_id', //category_id is the foreign key for products
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, { //a category has many products
+  foreignKey: 'category_id', //category_id is the foreign key
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(ProductTag, {
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+ProductTag.belongToMany(Product, {
+  foreignKey: 'tag_id',
+});
 module.exports = {
   Product,
   Category,
